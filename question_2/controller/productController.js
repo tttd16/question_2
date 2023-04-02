7b36fce7b84b599206fdf18a17f1a4044208655d
tttd16 <tiendung.work16@gmail.com>
2023-04-02 20:39:05 +0700

    testky3

--- question_2/controller/productController.js
+++ question_2/controller/productController.js
@@ -6,42 +6,15 @@ const getAllProduct = asyncHandler(async (req, res) => {
   res.json(products);
 });
 
-const fillterProduct = asyncHandler(async (req, res) => {
+const filterProduct = asyncHandler(async (req, res) => {
   const fillProduct = await productModel.find({});
   const fillterP = fillProduct.filter(e => e.instock < 100)
   res.json(fillterP);
 });
 
 
-async function getOrderList(req, res) {
-  try {
-    const orders = await Order.aggregate([
-      {
-        $lookup: {
-          from: 'inventory',
-          localField: 'item',
-          foreignField: 'sku',
-          as: 'product'
-        }
-      },
-      {
-        $project: {
-          _id: 1,
-          item: 1,
-          price: 1,
-          quantity: 1,
-          description: { $arrayElemAt: ['$product.description', 0] }
-        }
-      }
-    ]).exec();
-
-    res.json(orders);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-}
 
 module.exports = {
   getAllProduct,
-  fillterProduct,
+  filterProduct,
 };
--- question_2/routes/product.js
+++ question_2/routes/product.js
@@ -2,12 +2,12 @@ const express = require("express");
 const productRouter = express.Router();
 const {
   getAllProduct,
-  fillterProduct,
+  filterProduct,
 } = require("../controller/productController");
 
 
 productRouter.get("/", getAllProduct);
 
-productRouter.get("/fill", fillterProduct);
+productRouter.get("/fill", filterProduct);
 
 module.exports = productRouter;
